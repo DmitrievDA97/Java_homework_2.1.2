ecd0dc373264ad461834ba377d47780581ccaddd
Denis Dmitriev <denitos97@gmail.com>
2021-01-19 10:52:07 +0300

    Update app

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 200;
-        int replenishment_amount = 1800;
+        int replenishmentAmount = 1_800;
         int limit = 1000;
-        if (replenishment_amount > limit) {
-            balance = balance + replenishment_amount + replenishment_amount / 100;
+        if (replenishmentAmount > limit) {
+            balance = balance + replenishmentAmount + replenishmentAmount / 100;
         } else {
-            balance = balance + replenishment_amount;
+            balance = balance + replenishmentAmount;
         }
         System.out.println(balance);
     }
